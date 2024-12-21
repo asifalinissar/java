240550524033db0b24308fd698e091829ae9ecb1
Asif ali nissar <asifnizar607@gmail.com>
2024-12-21 15:35:22 +0530

    Add files via upload

---
+++ Helloworld_/src/newworld/HelloWorld_test.java
@@ -0,0 +1,20 @@
+package newworld;
+
+import java.util.Scanner;
+
+public class HelloWorld_test {
+	
+	public static void main (String [] args ) {
+	
+		
+		Scanner num =  new Scanner(System.in);
+		System.out.println("enter a number to find factorial");
+		
+		int number = num.nextInt();
+		System.out.println(number);
+		
+		
+		
+	}
+
+}
